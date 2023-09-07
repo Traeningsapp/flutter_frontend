a466159014d86ea537234a49dd4c753ec0c7ec30
Alligo324 <54527833+Alligo324@users.noreply.github.com>
2023-09-07 14:16:20 +0200

    Color scheme and more (#30)
    * Added colorscheme, fixing riveanimation at the moment
    
    * Design, color scheme, NewWorkout paramaters
    
    * Another push to master

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -11,22 +11,25 @@ const Color SecondaryColor1 = Color.fromARGB(255,24,151,143);
 const Color TertiaryColor1 = Color.fromARGB(255,233,218,193);
 const Color HeadlineColor1 = Color.fromARGB(255,247,236,222);
 const Color TextColor1 = Color.fromARGB(255,247,236,222);
+const Color activationColor1 = Color.fromARGB(255,61,36,108);
 
 const Color MainColor2 = Color.fromARGB(255,83,113,136);
 const Color SecondaryColor2 = Color.fromARGB(255,203,178,121);
 const Color TertiaryColor2 = Color.fromARGB(255,225,212,187);
 const Color HeadlineColor2 = Color.fromARGB(255,238,238,238);
 const Color TextColor2 = Color.fromARGB(255,238,238,238);
+const Color activationColor2 = Color.fromARGB(255,61,36,108);
 
 const Color MainColor3 = Color.fromARGB(255,155,171,184);
 const Color SecondaryColor3 = Color.fromARGB(255,238,227,203);
 const Color TertiaryColor3 = Color.fromARGB(255,215,192,174);
 const Color HeadlineColor3 = Color.fromARGB(255,150,126,118);
 const Color TextColor3 = Color.fromARGB(255,150,126,118);
+const Color activationColor3 = Color.fromARGB(255,61,36,108);
 
 const Color MainColor4 = Color.fromARGB(255,255,219,195);
 const Color SecondaryColor4 = Color.fromARGB(255,159,145,204);
 const Color TertiaryColor4 = Color.fromARGB(255,92,75,153);
 const Color HeadlineColor4 = Color.fromARGB(255,70,25,89);
 const Color TextColor4 = Color.fromARGB(255,61,36,108);
-
+const Color activationColor4 = Color.fromARGB(255,61,36,108);
